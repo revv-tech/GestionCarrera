7002dfd8e8a46ec0eb34d25d3a9106fe5cc3fd0b
Marco Reveiz <revvace@gmail.com>
2022-02-20 15:54:35 -0600

    Docs Iniciales Done

--- GestionadorCarreras/src/gestionadorcarreras/controlador/Controlador.java
+++ GestionadorCarreras/src/gestionadorcarreras/controlador/Controlador.java
@@ -4,11 +4,91 @@
  */
 package gestionadorcarreras.controlador;
 
+import gestionadorcarreras.modelo.Carrera;
+import gestionadorcarreras.modelo.Formulario;
+import gestionadorcarreras.modelo.TEstado;
+import java.util.Collection;
+
 /**
  * 
  * @author Marco
  */
 public class Controlador {
-
+    
+    private GestorCarreras gCarreras;
+    private GestorFormularios gFormularios;
+    /**
+     * Metodo para crear carrera
+     * @param dto
+     * @return 
+     */
+    public boolean crearCarrera(DTOCarreras dto){
+        return false;
+    }
+    /**
+     * Metodo para visualizar la informacion de una carrera
+     * @return 
+     */
+    public Collection visualizarCarreras(){
+        return null;
+    }
+    /**
+     * Metodo para obtener carrera
+     * @param codigo
+     * @param codigoSede
+     * @return 
+     */
+    public Carrera getCarrera(String codigo, String codigoSede){
+        return null;
+    }
+    /**
+     * Metodo para editar carrera
+     * @param carrera
+     * @return 
+     */
+    public boolean editarCarrera(Carrera carrera){
+        return false;
+    }
+    /**
+     * Metodo para crear formulario
+     * @param unFormulario
+     * @return 
+     */
+    public boolean crearFormulario(Formulario unFormulario){
+        return false;
+    }
+    /**
+     * Metodo para simular aplicacion de examen
+     * @return 
+     */
+    public boolean simularAplicacionExamen(){
+        return false;
+    }
+    /**
+     * Metodo para procesar resultados
+     * @return 
+     */
+    public boolean procesarResultados(){
+        return false;
+    }
+    /**
+     * Metodo para visualizar las solicitudes
+     * @return 
+     */
+    public Collection visualizarSolicitudes(){
+        return null;
+    }
+    /**
+     * Metodo para ver formularios
+     * @param estado
+     * @param porNombre
+     * @return 
+     */
+    public Collection verFormularios(TEstado estado, boolean porNombre){
+        return null;
+    }
+    
+    
+    
     
 }
--- GestionadorCarreras/src/gestionadorcarreras/controlador/GestorCarreras.java
+++ GestionadorCarreras/src/gestionadorcarreras/controlador/GestorCarreras.java
@@ -4,11 +4,54 @@
  */
 package gestionadorcarreras.controlador;
 
+import gestionadorcarreras.modelo.Carrera;
+import gestionadorcarreras.modelo.Sede;
+import java.util.Collection;
+
 /**
  *
  * @author Marco
  */
 public class GestorCarreras {
-
     
+    /**
+     * Metodo para agregar una carrera
+     * @param unaCarrea
+     * @return 
+     */
+    public boolean agregarCarrera(Carrera unaCarrea){
+        return false;
+    }
+    /**
+     * Metodo para buscar carreras
+     * 
+     * @return 
+     */
+    public Collection buscarCarreras(){
+        return null;
+    }
+    /**
+     * Metodo de buscar sede
+     * @param codigo
+     * @return 
+     */
+    public Sede buscarSede(String codigo){
+        return null;
+    }
+    /**
+     * Metodo para buscar carrera por codigo
+     * 
+     * @return 
+     */
+    public Carrera buscarCarrera(String codigo){
+        return null;
+    }
+    /**
+     * Metodo para modificar carrera
+     * @param unaCarrera
+     * @return 
+     */
+    public boolean modificarCarrera(Carrera unaCarrera){
+        return false;
+    }
 }
