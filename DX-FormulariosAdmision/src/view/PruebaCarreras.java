6efb584f092e543b39552b8fab1b90b3515febbe
Viviana Acosta <acostavivianacr@estudiantec.cr>
2022-02-28 22:21:02 -0600

    Otras pruebas

--- DX-FormulariosAdmision/src/view/PruebaCarreras.java
+++ DX-FormulariosAdmision/src/view/PruebaCarreras.java
@@ -1,19 +1,15 @@
 package view;
 
 import controller.Control;
-import controller.DAO.DAOCarrerasImpl;
-import controller.DAO.DAOSedesImpl;
 import controller.DTOCarrera;
 import controller.DTOFormulario;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import model.Carrera;
 import model.Formulario;
 import model.Sede;
 import model.TEstado;
-import model.TGrado;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,6 +21,7 @@ import model.TGrado;
  *
  * @author ersolano
  */
+
 public class PruebaCarreras {
  
     static DTOCarrera elDTO;
@@ -83,6 +80,66 @@ public class PruebaCarreras {
         System.out.println("Código de sede = " + codigoSede);
         System.out.println("\n");
     }    
+    
+    private static void generarFormularios(){
+        // Ejemplo de agregar formularios
+        ArrayList<String> correos = new ArrayList<>();
+        correos.add("shakemnat@proxy4gs.com");
+        correos.add("eap420@mexcool.com");
+        correos.add("ph4ntom14@bookew.site");
+        correos.add("cbreeze@googl.win");
+        correos.add("t4f8b578@cggup.com");
+        correos.add("yavkachev91@gasssmail.com");
+        
+        ArrayList<String> telefonos = new ArrayList<>();
+        telefonos.add("10293847");
+        telefonos.add("99775647");
+        telefonos.add("17263846");
+        telefonos.add("09172836");
+        telefonos.add("62438769");
+        
+        ArrayList<String> ids = new ArrayList<>();
+        ids.add("123872637");
+        ids.add("238923392");
+        ids.add("123942347");
+        ids.add("120947382");
+        ids.add("120397128");
+        
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("Juan Perez");
+        nombres.add("Andrés Acosta");
+        nombres.add("Diana Rodriguez");
+        nombres.add("Benita Seijas");
+        nombres.add("Luis Romero");
+
+
+
+        for (int i = 0 ; i < 5 ; i++){
+            // Carrera de prueba
+            // Maximo y minimo de index de carreras
+            int min = 0;
+            int max = elDTO.getLasCarreras().size()-1;
+            int carreraAleatoria = (int)(Math.random()*(max-min+1)+min);
+            Carrera carrera = elDTO.getLasCarreras().get(carreraAleatoria);
+            // Empieza a crear el DTO (la nota siempre empezara en cero y el gestor poner el num del formulario)
+            dtoFormulario.setCarreraSolicitada(carrera);
+            dtoFormulario.setCorreo(correos.get(i));
+            dtoFormulario.setEdad((int) Math.floor(Math.random()*(100-16+1)+16));
+            dtoFormulario.setEstado(TEstado.SOLICITANTE);
+            Date date = new Date();
+            date.setYear((int) Math.floor(Math.random()*(106-80+1)+80));
+            date.setMonth((int) Math.floor(Math.random()*(11-0+1)+0));
+            date.setDate((int) Math.floor(Math.random()*(28-1+1)+1));
+            dtoFormulario.setFechaNacimiento(date);
+            dtoFormulario.setIdentificacion(ids.get(i));
+            dtoFormulario.setInstSecundaria("New Hope Highschool");
+            dtoFormulario.setNombreCompleto(nombres.get(i));
+            dtoFormulario.setSede(carrera.getLaSede());
+            dtoFormulario.setTelefono(telefonos.get(i));
+            dtoFormulario.setDireccionResidencia("Heredia, San Rafel");
+            Control.getInstance().crearFormulario(dtoFormulario);
+        }
+    }
 
     public static void main(String[] args) {
         // se activa el DTO que provee el control
@@ -138,32 +195,7 @@ public class PruebaCarreras {
         Sede sede = new Sede("LI","Limon");
         
         System.out.println("Generando formularios...");
-        
-        // Ejemplo de agregar formularios
-        for (int i = 10 ; i != 0 ; i--){
-            // Carrera de prueba
-            // Maximo y minimo de index de carreras
-            int min = 0;
-            int max = elDTO.getLasCarreras().size()-1;
-            int carreraAleatoria = (int)(Math.random()*(max-min+1)+min);
-            Carrera carrera = elDTO.getLasCarreras().get(carreraAleatoria);
-            // Empieza a crear el DTO (la nota siempre empezara en cero y el gestor poner el num del formulario)
-            dtoFormulario.setCarreraSolicitada(carrera);
-            dtoFormulario.setCorreo("revvace@gmail.com");
-            dtoFormulario.setEdad(20);
-            dtoFormulario.setEstado(TEstado.ACEPTADO);
-            Date date = new Date();
-            dtoFormulario.setFechaNacimiento(date);
-            dtoFormulario.setIdentificacion("117680133");
-            dtoFormulario.setInstSecundaria("New Hope Highschool");
-            dtoFormulario.setNombreCompleto("Marco Antonio Reveiz Rojas");
-            dtoFormulario.setSede(sede);
-            dtoFormulario.setTelefono("85769188");
-            dtoFormulario.setDireccionResidencia("Heredia, San Rafel");
-            Control.getInstance().crearFormulario(dtoFormulario);
-        }
-        //Control.getInstance().mostrarFormularios();
-        //Simula examen de nota
+        generarFormularios();
         
         System.out.println("Aplicando exámenes...");
         Control.getInstance().simularExamen();
@@ -176,7 +208,46 @@ public class PruebaCarreras {
         showFormularios((ArrayList)dtoFormulario.getFormularios());
         
         System.out.println("Mostrando las solicitudes RECHAZADAS por nombre del solicitante");
-        Control.getInstance().
+        Control.getInstance().ordenarXNombre();
+        showFormularios((ArrayList)dtoFormulario.getFormularios());
+
+        System.out.println("Mostrando las solicitudes RECHAZADAS por nota obtenida del solicitante");
+        Control.getInstance().ordenarXPuntaje();
+        showFormularios((ArrayList)dtoFormulario.getFormularios());
         
+                System.out.println("Mostrando las solicitudes RECHAZADAS por nombre del solicitante");
+        Control.getInstance().ordenarXNombre();
+        showFormularios((ArrayList)dtoFormulario.getFormularios());
+
+        System.out.println("Mostrando las solicitudes RECHAZADAS por nota obtenida del solicitante");
+        Control.getInstance().ordenarXPuntaje();
+        showFormularios((ArrayList)dtoFormulario.getFormularios());
+        
+        System.out.println("Mostrando el total de solicitudes aceptadas...");
+        dtoFormulario.setCarreraSolicitada(null);
+        dtoFormulario.setSede(null);
+        dtoFormulario.setEstado(TEstado.ACEPTADO);
+        Control.getInstance().totalSolicitudesEstado(dtoFormulario);
+        System.out.println(dtoFormulario.getTotalEstado());  
+        
+        System.out.println("Mostrando el total de solicitudes rechazadas...");
+        dtoFormulario.setCarreraSolicitada(null);
+        dtoFormulario.setSede(null);
+        dtoFormulario.setEstado(TEstado.RECHAZADO);
+        Control.getInstance().totalSolicitudesEstado(dtoFormulario);
+        System.out.println(dtoFormulario.getTotalEstado());        
+            
+        
+        System.out.println("Mostrando el total de solicitudes en espera...");
+        dtoFormulario.setCarreraSolicitada(null);
+        dtoFormulario.setSede(null);
+        dtoFormulario.setEstado(TEstado.EN_ESPERA);
+        Control.getInstance().totalSolicitudesEstado(dtoFormulario);
+        System.out.println(dtoFormulario.getTotalEstado());
+
+        System.out.println("Mostrando los datos del formulario con id 4");
+        dtoFormulario.setNum(4);
+        Control.getInstance().verUnFormulario(dtoFormulario);
+
     }
 }
